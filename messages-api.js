6c87957ee22bd2492757aa13c8b806189ce51ace
endlaz <lazlovdende@hotmail.com>
2019-09-28 22:58:19 +0200

    Add request limit

--- messages-api.js
+++ messages-api.js
@@ -2,8 +2,25 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
+const reqLimit = 5;
+let reqRemaining = reqLimit;
 
-app.use(bodyParser.json());
+const reqMiddleware = (req, res, next) => {
+    console.log(reqRemaining);
+    if (reqRemaining > 0) {
+        reqRemaining = reqRemaining - 1;
+        return next();
+    } else {
+        resetReqRemaining();
+        return res.status(429).send('Requests blocked for 60 seconds');
+    }
+}
+const resetReqRemaining = () => {
+    console.log('time out set')
+    setTimeout(() => { reqRemaining = reqLimit; console.log('Request limit reset') }, 60000);
+}
+
+app.use(bodyParser.json(), reqMiddleware);
 
 app.post('/messages', (req, res) => {
     const { text } = req.body;
